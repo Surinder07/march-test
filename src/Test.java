14ec3c235a1d22597a5a00eace795e0f59e5a84a
Ravneet Sohal <ravneetsohal@Ravneets-MacBook-Air.local>
2024-04-11 21:53:37 -0400

    Made some changes.

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,8 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        System.out.println(3 + 5);
+
     }
 }
 
-// git clone https://github.com/Surinder07/march-test.git
-// adding something in main branch
