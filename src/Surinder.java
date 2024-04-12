da81f7d17651cf5af43029a385897f369dea6cd8
surinder0322@gmail.com <sunny@Sunnys-Laptop.local>
2024-04-11 21:57:03 -0400

    added something

--- src/Surinder.java
+++ src/Surinder.java
@@ -2,6 +2,7 @@ public class Surinder {
     public static void main(String[] args) {
         System.out.println("Testing something in the class");
         System.out.println("One more change");
+        System.out.println("Some more feature....");
 
     }
 }
